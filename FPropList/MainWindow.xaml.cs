322af591f2a265fd60af7087eabe5662d0cb58bc
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2019-08-04 23:47:10 +0530

    Some additions

--- FPropList/MainWindow.xaml.cs
+++ FPropList/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +26,26 @@ namespace FPropList
         public MainWindow()
         {
             InitializeComponent();
+            this.txtFolderPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            lvFolderList.ItemsSource = GetFileList(this.txtFolderPath.Text);
         }
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.ShowDialog();
+
+            this.txtFolderPath.Text = folderDialog.SelectedPath;
+        }
+
+
+        private string[] GetFileList(string folderPath)
+        {
+            return Directory.GetDirectories(folderPath);
+        }
+
     }
 }
